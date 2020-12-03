d2cce58fb0ce04459869f7aa2bdbe91946dcdc4a
Mohammad Yunus <mohammadyunus102@gmail.com>
2020-12-03 12:03:24 +0530

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
+//importing db models
+const shortUrls = require('./models/shortUrl')
 const app = express();
 
 mongoose.connect('mongodb:localhost/urlShortener',{
@@ -7,14 +9,21 @@ mongoose.connect('mongodb:localhost/urlShortener',{
     useUnifiedTopology: true
 })
 
+//sets template engine
 app.set('view engine', 'ejs')
+ 
+//makes it possible to use url params
+app.use(express.urlencoded({ extended: false }))
 
+//
 app.get('/', (req, res)=>{
     res.render('index')
 })
 
-app.post('/shortUrls', (req, res)=>{
-
+app.post('/shortUrls',async (req, res)=>{
+    await shortUrls.create({ full: req.body.fullUrl })
+    console;
+    res.redirect('/')
 })
 
 app.listen(process.env.PORT || 5001, ()=>{
